32d894613e7051930d06c176163982bc08dee447
Preet Panchal <preet2020@icloud.com>
2023-02-05 12:46:05 -0500

    connected

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -23,10 +23,13 @@ const Home = () => {
       setloading(true);
 
       try {
-        const response = await fetch("http://localhost:8080/api/v1/post", {
-          method: "GET",
-          headers: { "Content-Type": "application/json" },
-        });
+        const response = await fetch(
+          "https://chitraai.onrender.com/api/v1/post",
+          {
+            method: "GET",
+            headers: { "Content-Type": "application/json" },
+          }
+        );
         if (response.ok) {
           const result = await response.json();
           setAllPosts(result.data.reverse());
