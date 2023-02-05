c3ead1e6d666f5f1fc477158c959b314394bf6ce
Preet Panchal <preet2020@icloud.com>
2023-02-05 14:12:18 -0500

    done

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -84,7 +84,7 @@ const CreatePost = () => {
       <div>
         <h1 className="font-extrabold text-[#222328] text-[32px] ">Create</h1>
         <p className="mt-2 text-[#666e75] text-[16px] maw-w-[500px]">
-          Create imaginative and visually stunning images through DALL-E AI and
+          Create imaginative and visually stunning images through Chitra AI and
           share them with the community
         </p>
       </div>
